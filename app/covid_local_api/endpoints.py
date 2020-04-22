6b2030cb0910f5bcde0b5f4eca3cb3ec8057a26a
Johannes Rieke <johannes.rieke@gmail.com>
2020-04-22 15:45:04 +0200

    Change /region endpoint to /geonames and redirect to geonames API

--- app/covid_local_api/endpoints.py
+++ app/covid_local_api/endpoints.py
@@ -3,13 +3,16 @@ import os
 import sys
 import time
 import datetime
-
+import geocoder
+import uvicorn
+import requests
+from starlette.responses import Response, RedirectResponse
 from fastapi import Depends, FastAPI, Query, status
-from starlette.responses import Response
 
 from covid_local_api.__version__ import __version__
 from covid_local_api.schema import HotlineList
 from covid_local_api.utils import endpoint_utils
+from covid_local_api import credentials
 
 # Initialize logger
 log = logging.getLogger(__name__)
@@ -25,11 +28,21 @@ app = FastAPI(
 
 
 @app.get(
-    "/region",
-    summary="Get region id via a free-form query."
+    "/geonames",
+    summary="Get geoname ids via free-form query. This redirects to the geonames API (https://www.geonames.org/export/web-services.html) but returns only results of feature class A (country, state, region) and P (city, village)."
 )
-def get_region_id(region_query: str = Query(..., description="Free-form query string.")):
-    return None
+async def get_geonames(
+    q: str = Query(..., description="Free-form query string."), 
+    maxRows: int = Query(10, description="Maximum number of entries to return.")
+):
+    response = RedirectResponse(url=
+        f"http://api.geonames.org/searchJSON?q={q}&maxRows={maxRows}&username={credentials.geonames_username}&featureClass=P&featureClass=A")
+    return response
+
+    # TODO: For now, this simply redirects to the geonames api, maybe parse the results instead and return only a subset.
+    #results = geocoder.geonames(region_query, key=credentials.geonames_username, maxRows=10, featureClass=['A', 'P'])
+    #print(results)
+    #return {"message": "not working yet"}
 
 @app.get(
     "/hotlines",
@@ -42,3 +55,8 @@ def get_hotlines(region_id: str = Query(..., description="Region ID to filter ho
 
 # Use function names as operation IDs
 endpoint_utils.use_route_names_as_operation_ids(app)
+
+
+# Run uvicorn server directly in here for debugging
+if __name__ == "__main__":
+    uvicorn.run(app)
