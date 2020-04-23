c7c32c73c0aa250e20ff6eca108bf0759157011e
Johannes Rieke <johannes.rieke@gmail.com>
2020-04-24 00:41:19 +0200

    Added method to database to get nearby places

--- app/covid_local_api/db_handler.py
+++ app/covid_local_api/db_handler.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sqlite3
 import urllib.request
+import math
 
 
 class DatabaseHandler():
@@ -41,5 +42,39 @@ class DatabaseHandler():
         # TODO: Maybe integrate hierarchy thing here directly, as I also handle distance calculations now in this class (see get_nearby).
         cur = self.con.execute(f"SELECT * FROM {sheet} WHERE geonames_id IN ({', '.join(map(str, geonames_ids))})")
         dicts = cur.fetchall()
+        return dicts
+
+    def get_nearby(self, sheet, lat, lon, max_distance=0.5, max_count=5):
+        """Returns nearby entries from `sheet` for a latitude/longitude pair, sorted by distance.
+
+        Note: Distance is right now not the true distance in kilometers, but the "distance" in degrees lat/lon
+            (i.e. sqrt((lat1-lat2)**2 + (lon1-lon2)**2)). This value is proportional to the kilometers at the 
+            equator but deviates the further you move to the poles. We cannot calculate the true distance 
+            because the sqlite database can't do complex math operations. 
+
+        Args:
+            sheet (str): The worksheet in the Google Sheet
+            lon (float or str): Longitude of the target location
+            lat (float or str): Latitude of the target location
+            max_distance (float, optional): Maximum distance to search for objects (in degrees lat/lon; default: 0.5)
+            max_count (float, optional): Maximum number of elements to return (default: 5). If more elements 
+                were found within `max_distance`, return the closest ones. 
+
+        Returns:
+            list of dict: Filtered database entries as key-value dicts
+        """
+        # Query elements from the sheet that are closer to lat/lon than max_distance, 
+        # order them by the distance, and limit number of rows to max_count.
+        # Distance is in degrees lat/lon, see comment in docstring.
+        # TODO: Find a better solution to calculate distances, based on true distance in kilometers. 
+        squared_distance = f"(lat-{lat})*(lat-{lat})+(lon-{lon})*(lon-{lon})"
+        query = f"SELECT *, {squared_distance} AS distance FROM test_sites WHERE {squared_distance} <= {max_distance}*{max_distance} ORDER BY {squared_distance} LIMIT {max_count}"
+        print(query)
+        cur = self.con.execute(query)
+        
+        dicts = cur.fetchall()
+        for d in dicts:
+            # Distance in SQL query is squared, so take the sqrt here. 
+            d["distance"] = math.sqrt(d["distance"])
         return dicts
 
